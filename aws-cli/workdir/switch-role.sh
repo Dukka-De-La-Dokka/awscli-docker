89b67ed9ef45a56f3813522351b6952384fba640
Dukka-De-La-Dokka <59502111+Dukka-De-La-Dokka@users.noreply.github.com>
2022-09-17 17:00:33 +0900

    version up 2.0

--- aws-cli/workdir/switch-role.sh
+++ aws-cli/workdir/switch-role.sh
@@ -9,8 +9,7 @@
 # getting credentials - key id, access key, and token
 AWS_STS_CREDENTIALS=$(aws sts assume-role \
 --role-arn arn:aws:iam::"${TARGET_ACCTID}":role/"${ROLE_NAME}" \
---role-session-name "${SESSION_NAME}" \
---duration-seconds "${DURSEC}")
+--role-session-name "${SESSION_NAME}")
 
 # extract the each credential using jq command
 AWS_ACCESS_KEY_ID=$(echo "${AWS_STS_CREDENTIALS}" | jq -r '.Credentials.AccessKeyId')
@@ -21,6 +20,3 @@ AWS_SESSION_TOKEN=$(echo "${AWS_STS_CREDENTIALS}" | jq -r '.Credentials.SessionT
 export AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY
 export AWS_SESSION_TOKEN
-
-# to switch back to the original iam
-#unset AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY AWS_SESSION_TOKEN
